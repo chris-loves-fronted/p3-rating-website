578327c025c2708d4b3c33c4a18d60e45617a875
chris-loves-frontend <cseyboth@mailbox.org>
2024-05-09 08:55:33 +0200

    Rating Functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// @ts-nocheck
 const reviewGrid = document.querySelector('main');
+const ratingButtons = document.querySelectorAll('input[name="rating"]');
+const reviewNameInput = document.getElementById('name');
+const reviewInput = document.getElementById('review');
+const submitButton = document.getElementById('submitButton');
+const reviewForm = document.querySelector('form');
+let newReview = {};
 
-const reviews = [
+let reviews = [
     {
         id: 1,
         name: 'Max Mustermann',
@@ -45,9 +52,6 @@ const reviews = [
     },
 ];
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded');
-    console.log(reviewGrid);
-
     for (const review of reviews) {
         const reviewCard = document.createElement('review-card');
         const leftSide = document.createElement('left-side');
@@ -55,8 +59,16 @@ window.addEventListener('DOMContentLoaded', () => {
         const userImage = document.createElement('img');
         const userName = document.createElement('h2');
         const userReview = document.createElement('p');
+        const reviewStarContainer = document.createElement('div');
+        const ratingContainer = document.createElement('rating-containter');
 
         reviewCard.setAttribute('data-id', String(review.id));
+
+        for (let i = Number(review.rating); i >= 1; i--) {
+            const reviewStars = document.createElement('span');
+            reviewStars.textContent = '⭐';
+            reviewStarContainer.appendChild(reviewStars);
+        }
 
         userImage.src = review.img;
         userImage.alt = '';
@@ -64,10 +76,77 @@ window.addEventListener('DOMContentLoaded', () => {
         userName.textContent = review.name;
         userReview.textContent = review.review;
 
+        ratingContainer.append(userName, reviewStarContainer);
+
         leftSide.appendChild(userImage);
-        rightSide.append(userName, userReview);
+        rightSide.append(ratingContainer, userReview);
 
         reviewCard.append(leftSide, rightSide);
         reviewGrid?.append(reviewCard);
     }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    ratingButtons.forEach((rating) => {
+        rating.addEventListener('click', () => {
+            newReview.rating = rating.value;
+        });
+    });
+
+    reviewNameInput.addEventListener('input', ({ target: { value } }) => {
+        newReview.name = value;
+    });
+
+    reviewInput.addEventListener('input', ({ target: { value } }) => {
+        newReview.review = value;
+    });
+
+    reviewForm.addEventListener('submit', (event) => {
+        if (!reviewForm.checkValidity()) return;
+
+        event.preventDefault();
+
+        newReview.id = reviews.length + 1;
+        newReview.img = 'https://i.pravatar.cc/50';
+        reviews = [newReview, ...reviews];
+
+        ratingButtons.forEach((rating) => (rating.checked = false));
+        reviewNameInput.value = '';
+        reviewInput.value = '';
+        reviewGrid.innerHTML = '';
+        newReview = {};
+
+        for (const review of reviews) {
+            const reviewCard = document.createElement('review-card');
+            const leftSide = document.createElement('left-side');
+            const rightSide = document.createElement('right-side');
+            const userImage = document.createElement('img');
+            const userName = document.createElement('h2');
+            const userReview = document.createElement('p');
+            const reviewStarContainer = document.createElement('div');
+            const ratingContainer = document.createElement('rating-containter');
+
+            reviewCard.setAttribute('data-id', String(review.id));
+
+            for (let i = Number(review.rating); i >= 1; i--) {
+                const reviewStars = document.createElement('span');
+                reviewStars.textContent = '⭐';
+                reviewStarContainer.appendChild(reviewStars);
+            }
+
+            userImage.src = review.img;
+            userImage.alt = '';
+
+            userName.textContent = review.name;
+            userReview.textContent = review.review;
+
+            ratingContainer.append(userName, reviewStarContainer);
+
+            leftSide.appendChild(userImage);
+            rightSide.append(ratingContainer, userReview);
+
+            reviewCard.append(leftSide, rightSide);
+            reviewGrid?.append(reviewCard);
+        }
+    });
 });
