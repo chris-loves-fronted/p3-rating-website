18a9489c7314bbb4dee45c3f0eec37f8228684bf
chris-loves-frontend <cseyboth@mailbox.org>
2024-05-12 09:15:19 +0200

    Loading Indicator

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { API } from './api/API.js';
 import { UI } from './ui/UI.js';
 
 //Global Variables
+const loadingSpinner = document.getElementById('loadingSpinner');
 const addReviewH2 = document.getElementById('addReview');
 const reviewGrid = document.querySelector('main');
 const reviewModalBox = document.querySelector('modal-box');
@@ -19,12 +20,14 @@ window.addEventListener('fetchError', () => {
     reviewGrid.innerHTML = `
       <h2>Error fetching Data, please try again later!</h2>
     `;
+    loadingSpinner.hidden = true;
 });
 
 // Fetch & Render Reviews
 document.addEventListener('DOMContentLoaded', async () => {
-    reviews = await API.fetchReviews();
-
+    reviews = [...(await API.fetchReviews())];
+    loadingSpinner.hidden = true;
+    
     UI.renderReviews(reviews, reviewGrid);
 });
 
